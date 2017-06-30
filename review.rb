4efa9cf63632a122c459cc71091f74386a7a46ca
nia-427 <ns0042@mix.wvu.edu>
2017-06-30 14:42:29 -0400

    Review codes

---
+++ review.rb
@@ -0,0 +1,36 @@
+# def random
+# 	array = rand(1..10)
+# p random
+#  if time > 
+
+
+# if array == 3
+# 	puts "You have won!"
+# else
+# 	puts "Try again"
+# end
+# end
+
+# number(3)
+
+
+def randomNumber(num)
+	array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].sample
+if array == num
+	puts "Win"
+
+elsif num > array
+p "Number is too high"
+
+elsif num < array
+p "Number is too low"
+
+end
+end
+
+randomNumber(3)
+
+
+
+
+
